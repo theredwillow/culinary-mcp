cd10586ea8e17e19e595fdd41077c3d08d127aaa
theredwillow <theredwillows@gmail.com>
2025-04-04 15:08:39 -0700

    Adding basic shopping list functionality

--- TODO.md
+++ TODO.md
@@ -1,16 +1,19 @@
 ## General
 - Does MCP have versioning?
+- Add bar code / SKU system, read grocery store receipts, etc...
 
 ## Config
 - Record user preferences like allergies and diet information
 
 ## Meal plans
+- On add: should add ingredients to shopping list?
 - Add meal plan prompt
 - Record meal suggestions and, if provided, whether the user liked them
 
 ## Shopping lists
-- Handle shopping list
+- On add: Is in inventory? Should remove from inventory or cancel?
 - Connect to grocery store API('s) to add aisle numbers
 
 ## Recipe book
 - Design a recipe storing system
+- Add plan meal function, adding to shopping list (new data: plans for ingredient)
--- mcp.py
+++ mcp.py
@@ -3,12 +3,13 @@
 from fastmcp import FastMCP
 from utils import get_list, add_to_list, remove_from_list
 
-mcp = FastMCP("Chef Consultant 👨‍🍳", dependencies=["utils"])
+mcp = FastMCP("Chef Consultant 👨‍🍳")
 
 # INVENTORY TOOLS
 
+
 @mcp.tool()
-def get_inventory() -> list:
+def get_inventory() -> str:
     """Get the inventory as a comma-separated list, such as fridge, pantry, cabinet, etc..."""
     return get_list("inventory")
 
@@ -23,3 +24,24 @@ def add_item_to_inventory(item: str) -> None:
 def remove_item_from_inventory(item: str) -> None:
     """Remove an item from the inventory, such as fridge, pantry, cabinet, etc..."""
     return remove_from_list("inventory", item)
+
+
+# SHOPPING LIST TOOLS
+
+
+@mcp.tool()
+def get_shopping_list() -> str:
+    """Get the shopping list as a comma-separated list"""
+    return get_list("shopping-list")
+
+
+@mcp.tool()
+def add_item_to_shopping_list(item: str) -> None:
+    """Add an item to the shopping list"""
+    return add_to_list("shopping-list", item)
+
+
+@mcp.tool()
+def remove_item_from_shopping_list(item: str) -> None:
+    """Remove an item from the shopping list"""
+    return remove_from_list("shopping-list", item)
